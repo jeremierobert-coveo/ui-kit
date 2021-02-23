0192697b71332fc353d0a8281b5d6a562fc39764
Sami Sayegh <ssayegh@coveo.com>
2021-02-23 18:22:32 -0500

    extract history controller documentation
    https://coveord.atlassian.net/browse/KIT-466

--- packages/headless/src/controllers/history/headless-history.ts
+++ packages/headless/src/controllers/history/headless-history.ts
@@ -1,20 +1,45 @@
 import {Engine} from '../../app/headless-engine';
-import {buildController} from '../controller/headless-controller';
+import {buildController, Controller} from '../controller/headless-controller';
 import {back, forward} from '../../features/history/history-actions';
 import {executeSearch} from '../../features/search/search-actions';
 import {
   logNavigateBackward,
   logNavigateForward,
 } from '../../features/history/history-analytics-actions';
+import {HistoryState as HistoryStateFeature} from '../../features/history/history-state';
+import {StateWithHistory} from '../../app/undoable';
 
 /**
  * The `History` controller is in charge of allowing navigating back and forward in the search interface history.
  */
-export type History = ReturnType<typeof buildHistory>;
-/** The state relevant to the `History` controller.*/
-export type HistoryState = History['state'];
+export interface History extends Controller {
+  /**
+   * The state of the `History` controller.
+   */
+  state: HistoryState;
 
-export const buildHistory = (engine: Engine) => {
+  /**
+   * Move backward in the interface history.
+   */
+  back(): Promise<void>;
+
+  /**
+   * Move forward in the interface history.
+   */
+  forward(): Promise<void>;
+}
+
+/**
+ * The state relevant to the `History` controller.
+ * */
+export type HistoryState = StateWithHistory<HistoryStateFeature>;
+
+/**
+ * Creates a `History` controller instance.
+ *
+ * @param engine - The headless engine.
+ */
+export function buildHistory(engine: Engine): History {
   const controller = buildController(engine);
   const {dispatch} = engine;
 
@@ -24,9 +49,6 @@ export const buildHistory = (engine: Engine) => {
       return engine.state.history;
     },
 
-    /**
-     * Move backward in the interface history.
-     */
     async back() {
       if (!this.state.past.length || !this.state.present) {
         return;
@@ -35,9 +57,6 @@ export const buildHistory = (engine: Engine) => {
       dispatch(executeSearch(logNavigateBackward()));
     },
 
-    /**
-     * Move forward in the interface history.
-     */
     async forward() {
       if (!this.state.future.length || !this.state.present) {
         return;
@@ -46,4 +65,4 @@ export const buildHistory = (engine: Engine) => {
       dispatch(executeSearch(logNavigateForward()));
     },
   };
-};
+}
